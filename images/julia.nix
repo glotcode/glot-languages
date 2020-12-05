c1f814cded8474bd4e6e9fbf469982d60462fff8
Petter Rasmussen <petter@hask.no>
2020-12-05 11:47:50 +0100

    Add julia

---
+++ images/julia.nix
@@ -0,0 +1,23 @@
+let
+  nixpkgs =
+    # Julia was marked as broken in stable so we get it from unstable
+    builtins.fetchGit {
+      url = "https://github.com/NixOS/nixpkgs";
+      ref = "refs/heads/nixos-unstable";
+      rev = "24eb3f87fc610f18de7076aee7c5a84ac5591e3e";
+    };
+
+  pkgs =
+    import nixpkgs {};
+
+  build_image =
+    import ./common/build_image.nix;
+in
+build_image {
+  pkgs = pkgs;
+  name = "glot/julia";
+  tag = "latest";
+  installedPackages = [
+    pkgs.julia
+  ];
+}
