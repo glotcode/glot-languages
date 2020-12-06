e7b8f951c54d531b26772134814242bda315911f
Petter Rasmussen <petter@hask.no>
2020-12-06 18:30:44 +0100

    Add rust

---
+++ images/rust.nix
@@ -0,0 +1,17 @@
+let
+  pkgs =
+    import ./common/nixpkgs.nix;
+
+  build_image =
+    import ./common/build_image.nix;
+in
+build_image {
+  pkgs = pkgs;
+  name = "glot/rust";
+  tag = "latest";
+  installedPackages = [
+    pkgs.binutils
+    pkgs.gcc
+    pkgs.rustc
+  ];
+}
