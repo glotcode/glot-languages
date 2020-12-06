7d6c41f386a883ddf6c1d1e224139c61c46d0b54
Petter Rasmussen <petter@hask.no>
2020-12-06 12:33:36 +0100

    Add typescript

---
+++ images/typescript.nix
@@ -0,0 +1,18 @@
+let
+  pkgs =
+    import ./common/nixpkgs.nix;
+
+  build_image =
+    import ./common/build_image.nix;
+in
+build_image {
+  pkgs = pkgs;
+  name = "glot/typescript";
+  tag = "latest";
+  installedPackages = [
+    pkgs.nodePackages.typescript
+  ];
+  env = [
+    "PATH=${pkgs.nodePackages.typescript}/bin/:${pkgs.nodejs}/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+  ];
+}
