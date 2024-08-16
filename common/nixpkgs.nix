fb475eea903728cfc161d83d39befacd58f361ba
Petter Rasmussen <petter@hask.no>
2024-08-16 17:18:43 +0200

    Update nixpkgs

--- common/nixpkgs.nix
+++ common/nixpkgs.nix
@@ -3,7 +3,7 @@ let
     builtins.fetchGit {
       url = "https://github.com/NixOS/nixpkgs";
       ref = "refs/heads/release-24.05";
-      rev = "abd29679271a9fbcffe1dd640fc6c2a77957f5ed";
+      rev = "8f4cb508c33212aa69ae22958d03c0ba9a906f5b";
     };
 
   pkgs =
